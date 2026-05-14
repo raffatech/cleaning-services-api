131074fdbb77aad2ab9303795f3b5211a96ae248
Rafaela Gomes dos Santos <rafaela.dsantos@cielo.com.br>
2026-05-14 11:22:31 -0300

    Inicio | Adicionar estrutura inicial do projeto meuCrud

---
+++ src/main/java/com/example/meuCrud/MeuCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.meuCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meuCrud/MeuCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meuCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
